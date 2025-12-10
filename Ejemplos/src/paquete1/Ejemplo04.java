870f0995dfcfdf91bca02533856a2203128411b8
CesarM3-A <cesarmateoab2007@gmail.com>
2025-12-10 12:07:14 -0500

    .length

--- Ejemplos/src/paquete1/Ejemplo04.java
+++ Ejemplos/src/paquete1/Ejemplo04.java
@@ -22,6 +22,8 @@ public class Ejemplo04 {
         int[] arreglo1 = {1000, 1, 2, 2000, 300};
         // System.out.println(arreglo1.length);
         // arreglo1[2] = 1000;
+        /* El .length sirve para traer el numero variables que tienen las 
+        posiciones del arreglo */
         for (int i = 0; i < arreglo1.length; i++) {
             System.out.printf("Subíndice o Índice %d, con valor %d\n",
                     i, arreglo1[i]);
